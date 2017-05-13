8384d3d6a4d8adbdb79fba4c1ff26cc783106e8b
Ondřej Ezr <oezr@msp.justice.cz>
2017-05-13 14:20:58 +0200

    initializers

--- lib/egov_utils/engine.rb
+++ lib/egov_utils/engine.rb
@@ -6,5 +6,19 @@ module EgovUtils
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
+    initializer "active_record.include_plugins" do
+      ActiveSupport.on_load(:active_record) do
+        include EgovUtils::HasAuditTrail
+      end
+    end
   end
 end
