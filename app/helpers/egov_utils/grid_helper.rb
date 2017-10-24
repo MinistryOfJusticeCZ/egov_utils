5b7c9b780671ac3d0a1642751237563635fe0fa9
Ondřej Ezr <oezr@msp.justice.cz>
2017-10-25 00:19:13 +0200

    translations

--- app/helpers/egov_utils/grid_helper.rb
+++ app/helpers/egov_utils/grid_helper.rb
@@ -1,10 +1,5 @@
 module EgovUtils
   module GridHelper
-    def grid_attributes_i18n_kyes(attribute)
-      keys = [ ('activerecord.attributes.'+attribute.model.model_name.i18n_key.to_s+'.'+attribute.name).to_sym ]
-      keys.concat( grid_attributes_i18n_kyes(attribute.attribute) ) if attribute.respond_to?(:attribute)
-      keys
-    end
 
     def type_for_grid(type)
       case type
@@ -35,7 +30,7 @@ module EgovUtils
     def column_for_grid(grid, attribute)
       s = "{"
       s << "field: '#{attribute.name}'"
-      s << ", title: '#{I18n.t('model_attributes.'+grid.model_i18n_key.to_s+'.'+attribute.name, default: grid_attributes_i18n_kyes(attribute))}'"
+      s << ", title: '#{attribute.attribute_name.human}'"
       s << ", columnTemplate: '<a href=\"#{polymorphic_path(grid.schema.model)}/{id}\">{#{attribute.name}}</div>'" if attribute.name == grid.schema.main_attribute_name
       if attribute.type == 'list'
         s << ", format: ( (value) -> I18n.t(value, {scope: 'activerecord.attributes.#{attribute.model.model_name.i18n_key}.#{attribute.name.to_s.pluralize}'}) ) "
