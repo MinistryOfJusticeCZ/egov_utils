13739bb44f3f157d9b53c51d78b812a0f30000fb
Ondřej Ezr <oezr@msp.justice.cz>
2018-04-24 17:17:24 +0200

    titles

--- db/migrate/20180424143207_add_titles_to_natural_people.rb
+++ db/migrate/20180424143207_add_titles_to_natural_people.rb
@@ -1,6 +1,6 @@
 class AddTitlesToNaturalPeople < ActiveRecord::Migration[5.1]
   def change
-    add_column :natural_people, :title, :string
-    add_column :natural_people, :higher_title, :string
+    add_column :egov_utils_natural_people, :title, :string
+    add_column :egov_utils_natural_people, :higher_title, :string
   end
 end
