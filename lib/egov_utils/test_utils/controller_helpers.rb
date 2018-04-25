0a825c933ed058e97bdcdae7e9fbd5f7df735466
Ondřej Ezr <oezr@msp.justice.cz>
2018-04-25 14:27:58 +0200

    test helper

--- lib/egov_utils/test_utils/controller_helpers.rb
+++ lib/egov_utils/test_utils/controller_helpers.rb
@@ -2,6 +2,8 @@ module EgovUtils
   module TestUtils
     module ControllerHelpers
 
+      attr_reader :signed_user
+
       def default_user(params={})
         user = FactoryBot.create(:egov_utils_user, params)
         user
@@ -16,12 +18,13 @@ module EgovUtils
       end
 
       def anonymous_user
-        u = default_user
+        user = default_user
         allow(user).to receive(:persisted?).and_return(false)
-        u
+        user
       end
 
       def sign_in(user = basic_user)
+        @signed_user = user
         allow(controller).to receive(:find_current_user).and_return(user)
       end
     end
