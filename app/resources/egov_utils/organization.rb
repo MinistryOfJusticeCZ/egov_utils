96533e1cf735152b97ace669ef87ded61b95ec0a
Ondřej Ezr <oezr@msp.justice.cz>
2019-04-18 00:40:10 +0200

    victims compensations testing support

--- app/resources/egov_utils/organization.rb
+++ app/resources/egov_utils/organization.rb
@@ -5,12 +5,20 @@ module EgovUtils
         new(id: 1, name: 'Městský soud 1', key: '104000', category_abbrev: 'MS' ),
         new(id: 2, name: 'Krajský soud 1', key: '204000', category_abbrev: 'KS' ),
         new(id: 3, name: 'Krajský soud 2', key: '205000', category_abbrev: 'KS' ),
-        new(id: 13, name: 'Okresní soud 1', key: '204001', category_abbrev: 'OS' )
+        new(id: 13, name: 'Okresní soud 1', key: '204001', category_abbrev: 'OS' ),
+        new(id: 14, name: 'Okresní soud v Chomutově', key: '205030', category_abbrev: 'OS' )
       ]
     end
 
     def self.find_by_key(key)
       all.detect{ |o| o.key == key }
+    end
+
+    def self.courts(organization_keys=nil)
+      all.select do |o|
+        %w{OS KS MS}.include?(o.category_abbrev) &&
+          (organization_keys.nil? || organization_keys.include?(o.key))
+      end
     end
 
     def self.region_courts(branches=false)
